8537b846f2ee724f4a7e1a9f873456e052cec764
PersonalInfoHidden <106556443+PersonalInfoHidden@users.noreply.github.com>
2024-10-24 20:33:29 +0200

    Reformed and added "tests","recent" and "random" tabs 🐌

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,34 @@ import Image from "next/image";
 import React from "react";
 import { Links, NavBar } from "./navbar";
 import { TestViewer } from "./testviewer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const ExampleComponent = ({ children }: { children?: React.ReactNode }) => {
     return <div>{children}</div>;
 };
 
+const testCategorys: Array<{ name: string; path: string }> = [
+    { name: "XYZ", path: "/random/xyz" },
+];
+
 export default function Home() {
     return (
-        <main className="flex flex-col items-center justify-between px-12">
-            <div className="grid grid-row-2 my-12">
-                <TestViewer />
-                <div>Få frågor slumpmässigt</div>
-            </div>
+        <main className="flex flex-col px-12">
+            <Tabs defaultValue="tests">
+                <TabsList>
+                    <TabsTrigger value="tests">Tests</TabsTrigger>
+                    <TabsTrigger value="random">Random</TabsTrigger>
+                    <TabsTrigger value="recent">Recents</TabsTrigger>
+                </TabsList>
+                <TabsContent value="tests">
+                    <div className="grid grid-row-2 my-12">
+                        <TestViewer />
+                    </div>
+                </TabsContent>
+                <TabsContent value="random">
+                    <div></div>
+                </TabsContent>
+            </Tabs>
         </main>
     );
 }
--- src/app/testviewer.tsx
+++ src/app/testviewer.tsx
@@ -1,6 +1,5 @@
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import Link from "next/link";
 import React from "react";
 import OptionsMenu from "./optionsmenu";
@@ -14,14 +13,14 @@ const avalibleTests = [
 
 export const TestViewer = () => {
     return (
-        <div className="bg-zinc-900  px-16 py-10 rounded-2xl border">
+        <div className="bg-zinc-900  px-8 py-10 rounded-2xl border">
             <div className="font-semibold text-lg my-2">
                 Välj mellan dessa Proven
             </div>
             <div className="grid grid-cols-1  justify-around">
                 {avalibleTests.map((value) => (
                     <div className="grid grid-cols-4 hover:bg-secondary p-2">
-                        <div className="leading-loose  px-6 ">
+                        <div className="leading-loose  pr-6 ">
                             <Link href={value.path}>{value.name}</Link>
                         </div>
                         <div className="col-span-2 flex items-center">
